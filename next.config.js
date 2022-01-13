7c5c8024f03b3965000d5e831928da087ec7243c
Aditya Jha <mail@adijha.com>
2022-01-13 20:10:33 +0530

    image-loader-added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-	reactStrictMode: true,
-	images: {
-		domains: ['dummyimage.com'],
+
+const withPlugins = require('next-compose-plugins')
+const optimizedImages = require('next-optimized-images')
+
+module.exports = withPlugins([
+	[
+		optimizedImages,
+		{
+			/* config for next-optimized-images */
+		},
+	],
+
+	{
+		reactStrictMode: true,
+		images: {
+			domains: ['dummyimage.com'],
+		},
 	},
-}
+	// your other plugins here
+])
